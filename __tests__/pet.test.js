7fba49dca0a75023b38730763cfb26a8239b5855
lwyers1@hotmail.co.uk <lwyers1@hotmail.co.uk>
2023-07-05 16:12:25 +0100

    pet.test.js

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -13,11 +13,6 @@ it('sets the name property', () => {
   expect(pet.name).toEqual('Fido');
 });
 
-it('has a initial age of 0', () => {
-  const pet = new Pet('Fido');
-  expect(pet.age).toEqual(0);
-});
-
 describe('growUp', () => {
   it('increments the age by 1 and return ', () => {
   const pet = new Pet('Fido');
@@ -26,4 +21,19 @@ describe('growUp', () => {
   expect(pet.age).toEqual(1);
 
 });
+});
+
+it('has a initial age of 0', () => {
+  const pet = new Pet('Fido');
+  expect(pet.age).toEqual(0);
+});
+
+it('has an inital hunger of 0', () => {
+const pet = new Pet('Fido');
+expect(pet.hunger).toEqual(0);
+});
+
+it('has an inital fitness of 10', () => {
+const pet = new Pet('Fido');
+expect(pet.fitness).toEqual(10);
 });
