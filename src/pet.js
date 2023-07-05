694eeccf7066cfabb14836475e9bc23c417a6e9e
lwyers1@hotmail.co.uk <lwyers1@hotmail.co.uk>
2023-07-05 16:13:08 +0100

    pet.js

--- src/pet.js
+++ src/pet.js
@@ -3,9 +3,14 @@ const { lstat } = require("graceful-fs");
 function Pet(name) {
     this.name = name;
     this.age = 0;
+    this.hunger = 0;
+    this.fitness = 10;
 }
 
 Pet.prototype.growUp = function() {
     this.age += 1;
+    this.hunger += 5;
+    this.fitness = this.fitness - 3;
 };
+
 module.exports = Pet;
